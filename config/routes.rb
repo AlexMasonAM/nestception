01fbefa1a0ad3b7cbeb86aa9ec79abbfc2544dd3
AlexMasonAM <alexmmason@gmail.com>
2015-01-20 18:14:20 -0800

    updated flowerpots controller and views with nested attributes from plant and flower

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :flowerpots do
-    resources :plants do
-      resources :flowers 
+  root 'flowerpots#index'
+  
+  resources :flowerpots do #flowerpots have material and cost field
+    resources :plants do #plants have name field
+      resources :flowers #have a color and smell field
     end
   end
 end
